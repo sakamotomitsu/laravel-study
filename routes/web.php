c7a34023cd60de2ec5fe128c7ea86c2249b46a50
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-10-23 00:45:43 +0900

    RESTルート追記

--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,7 @@ Route::get( 'board', 'BoardController@index' );
 
 Route::get( 'board/add', 'BoardController@add' );
 Route::post( 'board/add', 'BoardController@create' );
+
+
+/* RESTRoute */
+Route::resource('rest', 'RestappController');
