1f74da49258a7fc080c13dcf641a1cee12b51f81
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-10-28 22:32:37 +0900

    指定アドレステスト $user = factory(User::class) -> create();         $response = $this -> actingAs($user) -> get('/hello');         $response -> assertStatus(200); ここの部分が500で返ってくる 私には分からん

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -9,17 +9,15 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class HelloTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use DatabaseMigrations;
+
     public function testHello()
     {
-        $this->assertTrue(true);
+        /*$this->assertTrue(true);
 
         $arr = [];
         $this -> assertEmpty($arr);
@@ -29,5 +27,21 @@ class HelloTest extends TestCase
 
         $n = random_int(0, 100);
         $this -> assertLessThan(100, $n);
+        */
+
+        $this -> assertTrue(true);
+
+        $response = $this -> get('/');
+        $response -> assertStatus(200);
+
+        $response = $this -> get('/hello');
+        $response -> assertStatus(302);
+
+        $user = factory(User::class) -> create();
+        $response = $this -> actingAs($user) -> get('/hello');
+        $response -> assertStatus(200);
+
+        $response = $this -> get('/no_route');
+        $response -> assertStatus(404);
     }
 }
